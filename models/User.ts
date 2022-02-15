87493009e75716d4513b6e46546cfd56d54ae67c
Omer <omer@Omer-Mac-mini.local>
2022-02-16 02:37:27 +0300

    added: auth middleware file

--- models/User.ts
+++ models/User.ts
@@ -1,68 +1,11 @@
 import { ObjectID } from "@bluelibs/mongo-bundle";
 
-export type TodoType = {
+export type UserType = {
   _id: ObjectID;
-  title: string;
-  status: string;
-  createDate: number;
+  // ...
 }
 
-export class Todo {
+export class User {
 
-  private _id?: ObjectID;
-  private title?: string;
-  private status?: string;
-  private createDate?: number;
-
-  constructor(_id?: ObjectID, title?: string, status?: string, createDate?: number) {
-    this._id = _id || new ObjectID();
-    this.title = title || "";
-    this.status = status || "pending";
-    this.createDate = createDate || Date.now();
-  }
-
-  public fromJSON(requestBody: any): Todo {
-    this._id = requestBody._id ?? new ObjectID();
-    this.title = requestBody.title ?? "";
-    this.status = requestBody.status ?? "pending";
-    this.createDate = requestBody.createDate ?? Date.now();
-    return this;
-  }
-
-  public asJSON(): any {
-    return {
-      title: this.title,
-      status: this.status,
-      createDate: this.createDate,
-    }
-  }
-
-  public getID(): ObjectID {
-    return this._id!;
-  }
-  public setID(newID: ObjectID): void {
-    this._id = newID;
-  }
-
-  public getTitle(): string {
-    return this.title!;
-  }
-  public setTitle(newTitle: string): void {
-    this.title = newTitle;
-  }
-
-  public getStatus(): string {
-    return this.status!;
-  }
-  public setStatus(newStatus: string): void {
-    this.status = newStatus;
-  }
-
-  public getCreateDate(): number {
-    return this.createDate!;
-  }
-  public setCreateDate(newCreateDate: number): void {
-    this.createDate = newCreateDate;
-  }
 
 }
--- services/Service.tokens.ts
+++ services/Service.tokens.ts
@@ -4,8 +4,6 @@ import {
 } from '@bluelibs/core';
 
 import { TodoService } from './Todo.service';
-import { DatabaseService } from './Database.service';
 
 export const CONTAINER_TOKEN = new Token<ContainerInstance>();
 export const TODO_SERVICE_TOKEN = new Token<TodoService>();
-export const DB_SERVICE_TOKEN = new Token<DatabaseService>();
